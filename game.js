c2f6cbccc4be995cca4686dd199e3b2f193f6c89
yishain11 <yishain11@walla.co.il>
2024-09-24 10:29:07 +0300

    added card class detailes

--- game.js
+++ game.js
@@ -1,8 +1,18 @@
+class Card {
+    constructor(value, suite) {
+        this.value = value;
+        this.suite = suite;
+        this.isFlipped = false;
+    }
+    showCard() {
+        console.log(`${this.value}${this.suite}`);
+    }
+}
+
 class Player { }
 
 class Deck { }
 
-class Card { }
 
 class Pile { }
 
