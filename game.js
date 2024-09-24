495faa9b9b71809fac64a0d4463d86cc8fcb8231
yishain11 <yishain11@walla.co.il>
2024-09-24 10:35:54 +0300

    added gen cards to deck method

--- game.js
+++ game.js
@@ -15,6 +15,17 @@ class Deck {
     constructor() {
         this.cards = [];
     }
+    generateDeck() {
+        // generate 52 cards - 1-13, 4 suites
+        const suites = ["d", "s", "c", "h"];
+        const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+        for (let i = 0; i < values.length; i++) {
+            const value = values[i];
+            suites.forEach(suite => {
+                this.cards.push(`${value}${suite}`);
+            });
+        }
+    }
     shuffleDeck() { }
 }
 
